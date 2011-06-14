dfa424173cc0bab8a5aca3a87e823cb712a5a668
Daniel Lo <dlo.caltech@gmail.com>
2011-06-14 17:47:10 -0400

    dnd: Major changes.

--- dnd/dnd.py
+++ dnd/dnd.py
@@ -11,16 +11,66 @@ class Character:
     # Save passed parameters
     self.name = name
     self.lvl = lvl
-    self.abilities = {'str' : str,
-                      'con' : con,
-                      'dex' : dex,
-                      'int' : inl,
-                      'wis' : wis,
-                      'cha' : cha}
+    self.abilities = {"str" : str,
+                      "con" : con,
+                      "dex" : dex,
+                      "int" : inl,
+                      "wis" : wis,
+                      "cha" : cha}
     
+    # Initialize other parameters
+    # Speed
+    self.speed_base = 0
+    self.speed_armor = 0
+    self.speed_item = 0
+    self.speed_misc = 0
+    # Fort
     self.fort_class = 0
+    self.fort_feat = 0
+    self.fort_enh = 0
+    self.fort_misc1 = 0
+    self.fort_misc2 = 0
+    # Reflex
     self.ref_class = 0
+    self.ref_feat = 0
+    self.ref_enh = 0
+    self.ref_misc1 = 0
+    self.ref_misc2 = 0
+    # Will
     self.will_class = 0
+    self.will_feat = 0
+    self.will_enh = 0
+    self.will_misc1 = 0
+    self.will_misc2 = 0
+    # Skills 
+    # All possible skills
+    all_skills = ["Acrobatics", "Arcana", "Athletics", "Bluff", "Diplomacy",
+                  "Dungeoneering", "Endurance", "Heal", "History", "Insight", 
+                  "Intimidate", "Nature", "Perception", "Religion", "Stealth",
+                  "Streetwise", "Thievery"]
+    # Initialize dictionary
+    self.skills = {}
+    # Entries listed as ability modifier, trained, armor penalty, misc
+    for skill in all_skills:
+      self.skills[skill] = ["", False, 0, 0]
+    for skill in ["Athletics"]:
+      self.skills[skill][0] = "str"
+    for skill in ["Endurance"]:
+      self.skills[skill][0] = "con"
+    for skill in ["Acrobatics", "Stealth", "Thievery"]:
+      self.skills[skill][0] = "dex"
+    for skill in ["Arcana", "History", "Religion"]:
+      self.skills[skill][0] = "int"
+    for skill in ["Dungeoneering", "Heal", "Insight", "Nature", "Perception"]:
+      self.skills[skill][0] = "wis"
+    for skill in ["Bluff", "Diplomacy", "Intimidate", "Streetwise"]:
+      self.skills[skill][0] = "cha"
+    # Hit points at 1st level
+    self.hp_init = 0
+    # Hit points per level
+    self.hp_per_lvl = 0
+    # Healing surges per day
+    self.healing_surges = 0
 
   def half_lvl(self):
     """ Returns half of level rounded down. """
@@ -29,27 +79,53 @@ class Character:
   def abil_mod(self, abil):
     """ Returns ability modifier for the specified ability. """
     modifier = int((self.abilities[abil]-10)/2)
-    modifier += self.half_lvl()
     return modifier
       
   def fort(self):
     """ Returns fortitude. """
     abil = max(self.abil_mod('str'), self.abil_mod('con'))
-    return 10 + self.half_lvl() + abil + self.fort_class 
+    return 10 + self.half_lvl() + abil + self.fort_class + self.fort_feat + \
+      self.fort_enh + self.fort_misc1 + self.fort_misc2
     
   def ref(self):
     """ Returns reflex. """
     abil = max(self.abil_mod('dex'), self.abil_mod('int'))
-    return 10 + self.half_lvl() + abil + self.ref_class 
+    return 10 + self.half_lvl() + abil + self.ref_class + self.ref_feat + \
+      self.ref_enh + self.ref_misc1 + self.ref_misc2
     
   def will(self):
     """ Returns will. """
     abil = max(self.abil_mod('wis'), self.abil_mod('cha'))
-    return 10 + self.half_lvl() + abil + self.will_class 
+    return 10 + self.half_lvl() + abil + self.will_class + self.will_feat + \
+      self.will_enh + self.will_misc1 + self.will_misc2
     
   def initiative(self):
     """ Returns initiative modifier for character. """
     return self.half_lvl() + self.abil_mod('dex')
+    
+  def speed(self):
+    """ Returns movement speed. """
+    return self.speed_base + self.speed_armor + self.speed_item + self.speed_misc
+    
+  def skill_bonus(self, skill):
+    """ Returns bonus for specified skill. """
+    [abil, trained, armor, misc] = self.skills[skill]
+    bonus = self.half_lvl() + self.abil_mod(abil) + armor + misc
+    if trained:
+      bonus += 5
+    return bonus
+    
+  def hp(self):
+    """ Max HP. """
+    return self.hp_init + (self.lvl - 1) * self.hp_per_lvl
+   
+  def hp_bloodied(self):
+    """ Bloodied HP is half max HP. """
+    return int(self.hp()/2)
+    
+  def hp_surge_value(self):
+    """ Healing surge is quarter max HP. """
+    return int(self.hp()/4)
     
   def display(self):
     """
@@ -57,12 +133,18 @@ class Character:
     """
     print "**************"
     print self.name
+    print "Speed: %d" % (self.speed())
     print "Initiative: %s" % (self.initiative())
+    print "HP: %d, Bloodied: %d, Healing Surge: %d (%d per day)" % (self.hp(), self.hp_bloodied(), self.hp_surge_value(), self.healing_surges)
     print self.abilities
     for key in self.abilities.keys():
       print "%s: %d, " % (key, self.abil_mod(key)),
     print
     print "Fort: %d, Ref: %d, Will: %d" % (self.fort(), self.ref(), self.will())
+    print
+    max_skill_name = len("Dungeoneering")
+    for skill in sorted(self.skills.keys()):
+      print "%-*s: %s" % (max_skill_name, skill, self.skill_bonus(skill))
     print "**************"
 
 class Power:
--- dnd/dnd_test.py
+++ dnd/dnd_test.py
@@ -2,16 +2,40 @@ import dnd
 
 # Initialize character
 Brashman = dnd.Character("Brashman", 2, 14, 12, 11, 10, 13, 16)
-Brashman.display()
 
 # Dragonborn racial bonuses
+# +2 str, +2 cha
 Brashman.abilities['str'] += 2
 Brashman.abilities['cha'] += 2
+# Speed 6
+Brashman.speed_base = 6
+# +2 History, +2 Intimidate
+Brashman.skills['History'][3] = 2
+Brashman.skills['Intimidate'][3] = 2
+# Dragonborn Fury: +1 attack when bloodied
+# Dragconic Heritage: Healing surge value = 1/4 hp + con mod
+func_type = type(Brashman.hp_surge_value)
+def draconic_hp_surge_value(self):
+  return int(self.hp()/4) + self.abil_mod("con")
+Brashman.hp_surge_value = func_type(draconic_hp_surge_value, Brashman, dnd.Character)
+# Dragon Breath: ability
 
 # Class bonuses
+# Armor proficiencies: cloth, leather, hide, chainmail, scale, plate, light/heavy shield
+# Weapon proficiencies: Simple/military melee, simple ranged
+# +1 fort, ref & will
 Brashman.fort_class = 1
 Brashman.ref_class = 1
 Brashman.will_class = 1
+# 1st level hit points = 15 + con
+Brashman.hp_init = 15 + Brashman.abilities["con"]
+# Hit points per level = 6
+Brashman.hp_per_lvl = 6
+# Healing surges per day = 10 + con
+Brashman.healing_surges = 10 + Brashman.abil_mod("con")
+# Trained skills: Religion + 3 of: Diplomacy, Endurance, Heal, History, Insight, Intimidate
+for skill in ["Religion", "Diplomacy", "Heal", "Insight"]:
+  Brashman.skills[skill][1] = True
 
 Brashman.display()
 
